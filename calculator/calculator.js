0ea40eb7222cda9fce54962819cfa2f387d7dbd1
Alex Parra <alexpds@gmail.com>
2019-10-24 00:03:28 +0100

    Solved calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,26 @@
-function add () {
-	
+function add (x, y) {
+	return x + y;
 }
 
-function subtract () {
-	
+function subtract (x, y) {
+	return x - y;
 }
 
-function sum () {
-	
+function sum (numbers) {
+	return numbers.reduce((acc, num) => acc + num, 0);
 }
 
-function multiply () {
-	
+function multiply (numbers) {
+	return numbers.reduce((acc, num) => acc * num, 1);
 }
 
-function power() {
-	
+function power(x, y) {
+	return x**y;
 }
 
-function factorial() {
-	
+function factorial(x) {
+	if( x < 0 ) throw new Error('Factorial does not work for negative numbers');
+	return (x === 0) ? 1 : x * factorial(x - 1);
 }
 
 module.exports = {
