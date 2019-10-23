cb75b27882cb3e840b80a9b4b072897f8a001ebc
Alex Parra <alexpds@gmail.com>
2019-10-24 00:10:41 +0100

    Solved fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+  x = Number(x);
+  if( isNaN(x) || x < 0 ) return 'OOPS';
+  return ( x < 2 ) ? x : fibonacci(x - 2) + fibonacci(x - 1);
 }
 
 module.exports = fibonacci
