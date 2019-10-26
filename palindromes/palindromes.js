f6948297fbdd57c2222a6aff7af4b7bdc3e6b30a
Alex Parra <alexpds@gmail.com>
2019-10-26 10:48:48 +0100

    Solved palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,4 +1,8 @@
-const palindromes = function() {
+const palindromes = function(text) {
+
+  const plainText = text.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  const plainReversed = plainText.split('').reduce((acc, l) => l + acc, '');
+  return plainText === plainReversed;
 
 }
 
