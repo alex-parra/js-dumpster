12208a11b7192785c8575a149f4404e6fcb1566d
Alex Parra <alexpds@gmail.com>
2019-10-28 22:11:14 +0000

    Solved pigLatin

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,9 +1,28 @@
-function translate() {
-	// body...
-}
+function translate(phrase) {
+  const words = phrase.split(' ');
+  const transformed = [];
+  words.forEach(word => {
+    let suffix = '';
+    for( let i = 0; i < word.length; i++ ) {
+      const c = word[i];
+      if( /[aeiou]/.test(c.toLowerCase()) ) {
+        suffix += 'ay';
+        transformed.push(word.substr(i) + suffix);
+        return;
+      }
 
+      if( 'q' === c.toLowerCase() && i < word.length - 1 && word[i+1].toLowerCase() === 'u' ) {
+        suffix += c + word[i+1];
+        i += 1;
+        continue;
+      }
+
+      suffix += c;
+    }
+  });
+  return transformed.join(' ');
+}
 
 module.exports = {
-	translate
-}
-
+  translate,
+};
