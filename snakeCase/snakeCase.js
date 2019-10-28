e41e7fc88d6dcc040610f8c15a2e73a7f52c1deb
Alex Parra <alexpds@gmail.com>
2019-10-28 22:43:17 +0000

    Solved snakeCase

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,9 @@
-const snakeCase = function() {
-
-}
+const snakeCase = function(phrase) {
+  let snakeCase = phrase.replace(/\.{2,}|-/g, ' ').replace(/[^a-zA-Z0-9\s-]/g, '');
+  if( snakeCase.indexOf(' ') === -1 ) {
+    snakeCase = snakeCase.replace(/([a-z])([A-Z])/g, '$1 $2');
+  }
+  return snakeCase.toLowerCase().trim().replace(/\s/g, '_');
+};
 
 module.exports = snakeCase
